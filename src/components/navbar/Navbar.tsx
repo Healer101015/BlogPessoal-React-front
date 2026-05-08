96aad4d2961029740812e6b7850d665e0d1f2a34
Healer101015 <josonjonesrids@gmail.com>
2026-05-08 12:14:54 -0300

    aa

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,11 +1,20 @@
-import { Link } from "react-router-dom";
+import { useContext } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { AuthContext } from "../../contexts/AuthContext";
 
 function Navbar() {
+    const navigate = useNavigate();
+    const { handleLogout } = useContext(AuthContext);
+
+    function logout() {
+        handleLogout();
+        alert("Usuário deslogado com sucesso!");
+        navigate("/login");
+    }
+
     return (
         <>
-            <div className='w-full flex justify-center py-4
-        bg-indigo-900 text-white'>
-
+            <div className='w-full flex justify-center py-4 bg-indigo-900 text-white'>
                 <div className="container flex justify-between text-lg mx-8">
                     <Link to='/home' className="text-2xl font-bold">Blog Pessoal</Link>
 
@@ -14,7 +23,9 @@ function Navbar() {
                         Temas
                         Cadastrar Tema
                         Perfil
-                        Sair
+                        <Link to="" onClick={logout} className="hover:underline">
+                            Sair
+                        </Link>
                     </div>
                 </div>
             </div>
